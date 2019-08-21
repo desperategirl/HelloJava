dedca3a91fbef57a865d2e11185a8112cff19a21
desperategirl <gjlhh0205@naver.com>
2019-08-21 13:30:30 +0900

    first

---
+++ src/GoodClass.java
@@ -0,0 +1,49 @@
+
+public class GoodClass {
+  //System.out.println("Good Morning")
+	
+	public static void main(String[] args) {
+		
+		// 괄호 안에 포함된 어떤 것들을 console에 보여라
+		// 하는 명령문
+		System.out.println("Good Morning");
+		
+		// 괄호 안에 아무것도 포함하지 않으면
+		// 그냥 빈 줄을 한 줄 표시하라
+		System.out.println();
+		System.out.println(30*40);
+		System.out.println(40/20);
+		System.out.println(30-20);
+		
+		// 따옴표로 묶이지 않은 숫자는
+		// 숫자 데이터, 숫자 리터럴이라고 부른다.
+		System.out.println(40+20);
+		// 숫자 데이터는 사칙연산을 수행할 수 있고
+		// 수학에서 사용하는 연산과 같은 역할을 한다.
+		
+		
+		
+		// 큰따옴표(double quotation)로 묶여 있는 어떤 것을
+		// 문자열 데이터, 문자열 리터럴이라고 부른다.
+		System.out.println("대한"+"민국");
+		// 문자열 데이터는 주로 덧셈 연산만 수행할 수 있고 
+		// 문자열들을 서로 연결시키는 역할만 수행한다.
+		
+		// 숫자 데이터이지만
+		// 큰 따옴표로 묶게 되면 
+		// 문자열이 되고 문자열 데이터와 같이 취급이 된다.
+		System.out.println("30"+"40");
+		
+		System.out.println("30+40");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
